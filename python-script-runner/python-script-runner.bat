d820e615cff042af234200c605c4d6f87e2e8059
andrewmilo <amiloslavsky@gmail.com>
2017-01-15 11:24:10 -0500

    add drag and drop functionality

--- python-script-runner/python-script-runner.bat
+++ python-script-runner/python-script-runner.bat
@@ -1,14 +1,19 @@
 @echo off
 
+set script=
+
+if NOT "%~1"=="" ( set script="%~1" & goto :params )
+
 echo "Enter the full script path, or just continue if there is only 1 local python script"
 
 set /p script=":"
 
+:params
 echo "Enter input parameters, separated by spaces"
 
 set /p params=":"
 
-if [%script%]==[] (
+if "%script%"=="" (
 
     for %%s in (*.*) do (
         
