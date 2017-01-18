04668cb7fd5bc447af26be9ac9194abb7b7c1116
andrewmilo <amiloslavsky@gmail.com>
2017-01-18 01:15:57 -0500

    fix choice bug

---
+++ quick-start/README.md
@@ -0,0 +1,2 @@
+# Quick-Start
+I have this script set to autorun when Windows loads up. At the press of a key, I'm able to instantly start up any applications that I register in the apps.txt file.
--- quick-start/launcher.bat
+++ quick-start/launcher.bat
@@ -7,6 +7,11 @@ choice /c %strings% /n
 echo %errorlevel%
 set /a level = "%errorlevel%-1"
 
-for /F "skip= %level% tokens=1,2,3 delims=," %%A in (apps.txt) do ( start %%C & exit >nul )
+set "skip=skip=%level%"
+echo %skip%
+
+if %level% LSS 1 ( set "skip=" & echo %skip% )
+
+for /F "%skip% tokens=1,2,3 delims=," %%A in (apps.txt) do ( start %%C & exit >nul )
 
 pause
